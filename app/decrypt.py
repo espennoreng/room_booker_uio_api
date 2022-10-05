c5783987fd5130a38418f810fd16264fc0d50b60
Your Name <espennoreng@gmail.com>
2022-10-05 23:44:40 +0200

    encryption

---
+++ app/decrypt.py
@@ -0,0 +1,32 @@
+import base64
+import hashlib
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+import os
+
+
+
+
+class Decrypt:
+    def __init__(self):
+        self.key = hashlib.sha256(os.environ['DECRYPT_KEY']).digest()
+
+    def encrypt(self, raw):
+        BS = AES.block_size
+        pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+        raw = base64.b64encode(pad(raw).encode('utf8'))
+        iv = get_random_bytes(AES.block_size)
+        cipher = AES.new(key= self.key, mode= AES.MODE_CFB,iv= iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        # dec = base64.b64decode(enc)
+        unpad = lambda s: s[:-ord(s[-1:])]
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CFB, iv)
+        return unpad(base64.b64decode(cipher.decrypt(enc[AES.block_size:])).decode('utf8'))
+        
+
+
+        
